4c3ab4400c2367cab99349be4435742fbed5d078
kasiaa224 <61841723+kasiaa224@users.noreply.github.com>
2020-04-17 20:58:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const osmosis = require('osmosis');
+
+osmosis
+    .get('https://www.google.com/search?q=webscraping&rlz=1C1SQJL_enUA897UA897&oq=webscraping&aqs=chrome..69i57j0l7.5612j0j7&sourceid=chrome&ie=UTF-8')
+    .find('#center_col')
+    .set({'related': ['.card-section .brs_col p a']})
+    .data(function(data) {
+        console.log(data);
+    })
